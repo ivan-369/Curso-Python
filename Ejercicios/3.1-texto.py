8d2c880d0677bf6765dc458519e103e97a787f57
ivan-369 <60492643+ivan-369@users.noreply.github.com>
2021-07-14 22:31:51 -0400

    Update 3.1-texto.py

--- Ejercicios/3.1-texto.py
+++ Ejercicios/3.1-texto.py
@@ -1,14 +1,14 @@
 """
 Crea un texto de al menos 10 caracteres
 """
-
+texto = "como estamos"
 
 """
 Imprime la letra que está en el índice 7
 """
-
+print(texto[6])
 
 """
 Imprime las letras que están en el rango desde la posición 3 a la 8
 """
-
+print(texto[2:8])
